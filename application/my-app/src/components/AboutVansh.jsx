604665615f76704d092f7c76d65934e7e1bc157d
Vansh Maheshbhai Virani <124015720+vvirani1908@users.noreply.github.com>
2025-02-23 14:50:28 -0800

    Vansh about me updated

--- application/my-app/src/components/AboutVansh.jsx
+++ application/my-app/src/components/AboutVansh.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
-import portrait from "./images/portrait.png"
+import portrait from "./images/VanshPortrait.png"
 import linkedin from "./images/linkedin1.png"
 import github from "./images/github1.png"
 
@@ -12,24 +12,25 @@ const AboutVansh= ({}) => {
         <>
         <div className="App"> 
                 <Header/>
-                <h1>Vansh here: Delete this h1 after updating</h1>
                 <div className="about-container">
                     <div className="about-info">
                         <div className='team-heading-container'>
-                            <div className='team-heading'>MARTHA MARTIN</div>
+                            <div className='team-heading'>VANSH VIRANI</div>
                             <div className='highlight-2'></div>
                         </div>
                         <div className="about-description">
                             <p>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-                                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            Hey there!
+                            Myself Vansh Virani, I am a Senior in Computer Science.
+                            I’m a big fan of cars—everything from classic muscle cars to modern sports cars. 
+                            I love the feeling of being behind the wheel and listening to the engine roar. 
+                            Most days, you’ll find me checking out new car models, talking shop with friends, and learning all I can about what makes each car unique. 
+                            When I’m not driving or dreaming about my next ride, I’m usually hanging out with fellow car enthusiasts, sharing tips and swapping stories. 
+                            Life is better when it’s spent in the fast lane.</p>
                         </div>
                         <div className="social-media"> 
-                            <a href="https://www.linkedin.com/in/martha-martin-pablo/" target="_blank"><img src={linkedin} alt="" /></a>
-                            <a href="https://github.com/mmartin440" target="_blank"><img src={github} alt="" /></a>
+                            <a href="www.linkedin.com/in/vansh-virani-10551a22a" target="_blank"><img src={linkedin} alt="" /></a>
+                            <a href="https://github.com/vvirani1908" target="_blank"><img src={github} alt="" /></a>
                             
                         </div>
                     </div>
