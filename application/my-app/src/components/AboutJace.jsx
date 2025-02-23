3476dda1982fb6199c4aef105dff4feba304cd0f
JaceB10 <jacebondoc03@gmail.com>
2025-02-22 21:15:03 -0800

    Edited and completed information on AboutJace (About me) page

--- application/my-app/src/components/AboutJace.jsx
+++ application/my-app/src/components/AboutJace.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
-import portrait from "./images/portrait.png"
+import portrait from "./images/jaceportrait.png"
 import linkedin from "./images/linkedin1.png"
 import github from "./images/github1.png"
 
@@ -12,24 +12,23 @@ const AboutJace= ({}) => {
         <>
         <div className="App"> 
                 <Header/>
-                <h1>Jace here: Delete this h1 after updating</h1>
                 <div className="about-container">
                     <div className="about-info">
                         <div className='team-heading-container'>
-                            <div className='team-heading'>MARTHA MARTIN</div>
+                            <div className='team-heading'>JAIDEEN-JACE BONDOC</div>
                             <div className='highlight-2'></div>
                         </div>
                         <div className="about-description">
                             <p>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-                                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                Jaideen-Jace is a student of SFSU who is currently studying for a degree in Computer Science. 
+                                Jaideen-Jace has been learning computer science for the past four years, brushing up on her knowledge of Java, Python, and C++. Originally an
+                                English major, she sought out this education with a spark of interest that gradually grew every year. Aside from Computer Science, she also has
+                                a minor degree in-the-works for Video Game Studies. She is looking forward to the excitement of learning and working with her teammates on a 
+                                new project together. For this project, Jaideen-Jace had been tasked with being one of the two front-end leads.</p>
                         </div>
                         <div className="social-media"> 
                             <a href="https://www.linkedin.com/in/martha-martin-pablo/" target="_blank"><img src={linkedin} alt="" /></a>
-                            <a href="https://github.com/mmartin440" target="_blank"><img src={github} alt="" /></a>
+                            <a href="https://github.com/JaceB10" target="_blank"><img src={github} alt="" /></a>
                             
                         </div>
                     </div>
