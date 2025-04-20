f1fb5ed6f082d36b776f3f0efe44b08872ac55b0
JaceB10 <jacebondoc03@gmail.com>
2025-04-20 00:03:33 -0700

    Created CourierPage.jsx, added route in App.js, and made additional changes to App.css relating to CourierPage.jsx

--- application/Frontend/src/App.js
+++ application/Frontend/src/App.js
@@ -9,6 +9,7 @@ import AboutJace from './components/AboutJace'
 import AboutVansh from './components/AboutVansh'
 import AboutTeam from './components/AboutTeam'
 import Chats from './components/Chats'
+import CourierPage from './components/CourierPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +25,7 @@ function App() {
         <Route path="/aboutVansh" element={<AboutVansh/>}/>
         <Route path='/aboutTeam' element={<AboutTeam/>}/>
         <Route path='/chats' element={<Chats/>} />
+        <Route path='/courierPage' element={<CourierPage/>} />
       </Routes>
     </BrowserRouter>
   )
