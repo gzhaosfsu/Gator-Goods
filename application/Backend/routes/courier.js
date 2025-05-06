e829c91a8a0688db14fdd57001ed78f6411b6f9e
AnthonyMingus <amingus@sfsu.edu>
2025-05-06 14:45:24 -0700

    Wrote the update for delivery_instruction

--- application/Backend/routes/courier.js
+++ application/Backend/routes/courier.js
@@ -10,46 +10,46 @@ router.get('/', (req, res) => {
     });
 });
 
-// GET courier by ID
-router.get('/:id', (req, res) => {
-    db.query('SELECT * FROM courier WHERE courier_id = ?', [req.params.id], (err, results) => {
-        if (err) return res.status(500).json({ error: err });
-        res.json(results[0]);
-    });
-});
+// // GET courier by ID
+// router.get('/:id', (req, res) => {
+//     db.query('SELECT * FROM courier WHERE courier_id = ?', [req.params.id], (err, results) => {
+//         if (err) return res.status(500).json({ error: err });
+//         res.json(results[0]);
+//     });
+// });
 
-// POST a new courier
-router.post('/', (req, res) => {
-    const { image, user_id, availability_status } = req.body;
-    db.query(
-        'INSERT INTO courier (image, user_id, availability_status) VALUES (?, ?, ?)',
-        [image, user_id, availability_status],
-        (err, result) => {
-            if (err) return res.status(500).json({ error: err });
-            res.status(201).json({ courier_id: result.insertId });
-        }
-    );
-});
+// // POST a new courier
+// router.post('/', (req, res) => {
+//     const { image, user_id, availability_status } = req.body;
+//     db.query(
+//         'INSERT INTO courier (image, user_id, availability_status) VALUES (?, ?, ?)',
+//         [image, user_id, availability_status],
+//         (err, result) => {
+//             if (err) return res.status(500).json({ error: err });
+//             res.status(201).json({ courier_id: result.insertId });
+//         }
+//     );
+// });
 
-// UPDATE a courier by ID
-router.put('/:id', (req, res) => {
-    const { image, user_id, availability_status } = req.body;
-    db.query(
-        'UPDATE courier SET image = ?, user_id = ?, availability_status = ? WHERE courier_id = ?',
-        [image, user_id, availability_status, req.params.id],
-        (err) => {
-            if (err) return res.status(500).json({ error: err });
-            res.sendStatus(204);
-        }
-    );
-});
+// // UPDATE a courier by ID
+// router.put('/:id', (req, res) => {
+//     const { image, user_id, availability_status } = req.body;
+//     db.query(
+//         'UPDATE courier SET image = ?, user_id = ?, availability_status = ? WHERE courier_id = ?',
+//         [image, user_id, availability_status, req.params.id],
+//         (err) => {
+//             if (err) return res.status(500).json({ error: err });
+//             res.sendStatus(204);
+//         }
+//     );
+// });
 
-// DELETE a courier by ID
-router.delete('/:id', (req, res) => {
-    db.query('DELETE FROM courier WHERE courier_id = ?', [req.params.id], (err) => {
-        if (err) return res.status(500).json({ error: err });
-        res.sendStatus(204);
-    });
-});
+// // DELETE a courier by ID
+// router.delete('/:id', (req, res) => {
+//     db.query('DELETE FROM courier WHERE courier_id = ?', [req.params.id], (err) => {
+//         if (err) return res.status(500).json({ error: err });
+//         res.sendStatus(204);
+//     });
+// });
 
 module.exports = router;
--- application/Backend/routes/vendor.js
+++ application/Backend/routes/vendor.js
@@ -10,46 +10,46 @@ router.get('/', (req, res) => {
     });
 });
 
-// GET vendor by ID
-router.get('/:id', (req, res) => {
-    db.query('SELECT * FROM vendor WHERE vendor_id = ?', [req.params.id], (err, results) => {
-        if (err) return res.status(500).json({ error: err });
-        res.json(results[0]);
-    });
-});
+// // GET vendor by ID
+// router.get('/:id', (req, res) => {
+//     db.query('SELECT * FROM vendor WHERE vendor_id = ?', [req.params.id], (err, results) => {
+//         if (err) return res.status(500).json({ error: err });
+//         res.json(results[0]);
+//     });
+// });
 
-// POST a new vendor
-router.post('/', (req, res) => {
-    const { rating, image, user_id } = req.body;
-    db.query(
-        'INSERT INTO vendor (rating, image, user_id) VALUES (?, ?, ?)',
-        [rating, image, user_id],
-        (err, result) => {
-            if (err) return res.status(500).json({ error: err });
-            res.status(201).json({ vendor_id: result.insertId });
-        }
-    );
-});
+// // POST a new vendor
+// router.post('/', (req, res) => {
+//     const { rating, image, user_id } = req.body;
+//     db.query(
+//         'INSERT INTO vendor (rating, image, user_id) VALUES (?, ?, ?)',
+//         [rating, image, user_id],
+//         (err, result) => {
+//             if (err) return res.status(500).json({ error: err });
+//             res.status(201).json({ vendor_id: result.insertId });
+//         }
+//     );
+// });
 
-// UPDATE a vendor by ID
-router.put('/:id', (req, res) => {
-    const { rating, image, user_id } = req.body;
-    db.query(
-        'UPDATE vendor SET rating = ?, image = ?, user_id = ? WHERE vendor_id = ?',
-        [rating, image, user_id, req.params.id],
-        (err) => {
-            if (err) return res.status(500).json({ error: err });
-            res.sendStatus(204);
-        }
-    );
-});
+// // UPDATE a vendor by ID
+// router.put('/:id', (req, res) => {
+//     const { rating, image, user_id } = req.body;
+//     db.query(
+//         'UPDATE vendor SET rating = ?, image = ?, user_id = ? WHERE vendor_id = ?',
+//         [rating, image, user_id, req.params.id],
+//         (err) => {
+//             if (err) return res.status(500).json({ error: err });
+//             res.sendStatus(204);
+//         }
+//     );
+// });
 
-// DELETE a vendor by ID
-router.delete('/:id', (req, res) => {
-    db.query('DELETE FROM vendor WHERE vendor_id = ?', [req.params.id], (err) => {
-        if (err) return res.status(500).json({ error: err });
-        res.sendStatus(204);
-    });
-});
+// // DELETE a vendor by ID
+// router.delete('/:id', (req, res) => {
+//     db.query('DELETE FROM vendor WHERE vendor_id = ?', [req.params.id], (err) => {
+//         if (err) return res.status(500).json({ error: err });
+//         res.sendStatus(204);
+//     });
+// });
 
 module.exports = router;
